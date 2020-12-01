793c416373f4feedfb10748ebe51eaa7f5a6b94e
Sven De Smet <xfg98855172@gmail.com>
2020-12-01 09:37:47 +0000

    Build for version 66

---
+++ precache-manifest.d3fb5bddeaf1e8ace5248b3479142437.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72ece70048d38348e3b5643ae75738d9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dfda98daa34972e0e5d0",
+    "url": "/static/js/2.08834a67.chunk.js"
+  },
+  {
+    "revision": "33e1595d6652746ad9cd7a6bf0e5f80e",
+    "url": "/static/js/2.08834a67.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b879f924b2a2625edde6",
+    "url": "/static/js/main.f4a44ecf.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
